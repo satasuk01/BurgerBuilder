cc75eaa168f0f28692c68e6ad54bf7032ca88644
Satasuk Viparksinlapin <satasuk@gmail.com>
2019-01-08 17:05:19 +0700

    add side drawer

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,16 +3,24 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.module.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 
 const sideDrawer = (props) => {
-    
-    return(
-        <div className={classes.SideDrawer}>
-            <div className={classes.Logo}><Logo/></div>
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
+
+    return (
+        <>
+            <Backdrop show={props.open} clicked={props.closed} />
+            <div className={attachedClasses.join(' ')}>
+                <div className={classes.Logo}><Logo /></div>
+                <nav>
+                    <NavigationItems />
+                </nav>
+            </div>
+        </>
     );
 };
 
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -7,8 +7,8 @@ import NavigationItems from '../NavigationItems/NavigationItems';
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
         <div>MENU</div>
-        <div className={classes.Logo}><Logo/></div>
-        <NavigationItems/>
+        <div className={classes.Logo}><Logo /></div>
+        <nav className={classes.DesktopOnly}><NavigationItems /></nav>
     </header>
 );
 
